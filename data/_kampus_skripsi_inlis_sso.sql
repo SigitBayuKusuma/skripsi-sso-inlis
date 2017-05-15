2bef42cc23adf4f2122175d8eded119e2365afec
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-15 19:50:45 +0700

    add database user and user_level

---
+++ data/_kampus_skripsi_inlis_sso.sql
@@ -0,0 +1,58 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.16-MariaDB : Database - _kampus_skripsi_inlis_sso
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`_kampus_skripsi_inlis_sso` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `_kampus_skripsi_inlis_sso`;
+
+/*Table structure for table `user_level` */
+
+DROP TABLE IF EXISTS `user_level`;
+
+CREATE TABLE `user_level` (
+  `level_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `publish` tinyint(1) NOT NULL DEFAULT '1',
+  `level_name` varchar(32) NOT NULL,
+  `level_desc` text NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`level_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_level` */
+
+insert  into `user_level`(`level_id`,`publish`,`level_name`,`level_desc`,`creation_date`) values (2,1,'admin','level admin','2017-05-15 19:18:20');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `publish` tinyint(1) NOT NULL DEFAULT '1',
+  `level_id` smallint(5) unsigned NOT NULL,
+  `member_id` int(11) unsigned NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  KEY `level_id` (`level_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`level_id`) REFERENCES `user_level` (`level_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
