9f9551950fcb7b991e24748449c0fbd285102ed1
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-23 18:14:59 +0700

    commit

--- data/_kampus_skripsi_inlis_sso.sql
+++ data/_kampus_skripsi_inlis_sso.sql
@@ -12,10 +12,6 @@ MySQL - 10.1.16-MariaDB : Database - _kampus_skripsi_inlis_sso
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`_kampus_skripsi_inlis_sso` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `_kampus_skripsi_inlis_sso`;
-
 /*Table structure for table `user_level` */
 
 DROP TABLE IF EXISTS `user_level`;
@@ -25,13 +21,14 @@ CREATE TABLE `user_level` (
   `publish` tinyint(1) NOT NULL DEFAULT '1',
   `level_name` varchar(32) NOT NULL,
   `level_desc` text NOT NULL,
+  `default` tinyint(1) NOT NULL DEFAULT '0',
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`level_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user_level` */
 
-insert  into `user_level`(`level_id`,`publish`,`level_name`,`level_desc`,`creation_date`) values (2,1,'admin','level admin','2017-05-15 19:18:20');
+insert  into `user_level`(`level_id`,`publish`,`level_name`,`level_desc`,`default`,`creation_date`) values (1,1,'administrator','administrator',0,'2017-05-23 17:30:28'),(2,1,'member','member inlis',1,'2017-05-23 17:30:43');
 
 /*Table structure for table `users` */
 
@@ -48,9 +45,11 @@ CREATE TABLE `users` (
   PRIMARY KEY (`user_id`),
   KEY `level_id` (`level_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`level_id`) REFERENCES `user_level` (`level_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 /*Data for the table `users` */
+
+insert  into `users`(`user_id`,`publish`,`level_id`,`member_id`,`email`,`password`,`creation_date`) values (1,1,1,0,'support@ommu.co','8d1169d1e270203727cc8d61f9b40452','2017-05-23 17:31:45');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
